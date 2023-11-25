bd03dc51c23c54667b37cdf367d9292507689ad2
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-11-25 21:39:04 +0900

    echoの導入

--- 1-editing/main.go
+++ 1-editing/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
@@ -14,9 +16,22 @@ func (h myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // main is the entry point for the application.
 func main() {
-	fmt.Println("Hello, World!")
+	e := echo.New()
 
-	handler := myHandler{}
-	http.Handle("/", handler)
-	http.ListenAndServe(":8080", nil)
+	e.Use(middleware.Logger())
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World from Echo!")
+	})
+
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(200, "Users!")
+	})
+
+	e.Start(":8080")
+
+	//fmt.Println("Hello, World!")
+	//
+	//handler := myHandler{}
+	//http.Handle("/", handler)
+	//http.ListenAndServe(":8080", nil)
 }
